c322a3b43b32f25049bb86389c510757e4b86353
mououkouou <flag326@hufs.ac.kr>
2023-12-18 13:46:03 +0900

    change woff init

--- src/views/ApprovalView.vue
+++ src/views/ApprovalView.vue
@@ -12,41 +12,30 @@ export default {
   },
   mounted() {
     /* eslint-disable */
-    woff
-      .init({
-        woffId: "6M4D9iS1o3oATtHNVJjS8w", // 발행된 WOFF ID
-      })
-      .then(() => {
-        console.log(woff.getOS());
-        if (!woff.isLoggedIn()) {
-          //로그인 유무 확인
-          window.alert("네이버웍스 로그인 후 사용 가능합니다.");
-          woff.closeWindow();
-          return;
-        } else {
-          woff.getProfile().then((v) => {
-            woff.openWindow({
-              url:
-                "https://test.commmmmmm/userId=" +
-                v.userId +
-                "&displayName=" +
-                v.displayName +
-                "&domainId=" +
-                v.domainId +
-                "&docId=" +
-                this.docInfo.docInfo +
-                "&formDocType=" +
-                this.docInfo.formDocType +
-                "&formURL=" +
-                this.docInfo.formURL,
-            });
-          });
-        }
-      })
-      .catch((err) => {
-        // 초기화 처리 중 오류가 발생한 경우
-        console.log(err.code, err.message);
+    if (!woff.isLoggedIn()) {
+      //로그인 유무 확인
+      window.alert("네이버웍스 로그인 후 사용 가능합니다.");
+      woff.closeWindow();
+      return;
+    } else {
+      woff.getProfile().then((v) => {
+        woff.openWindow({
+          url:
+            "https://test.commmmmmm/userId=" +
+            v.userId +
+            "&displayName=" +
+            v.displayName +
+            "&domainId=" +
+            v.domainId +
+            "&docId=" +
+            this.docInfo.docInfo +
+            "&formDocType=" +
+            this.docInfo.formDocType +
+            "&formURL=" +
+            this.docInfo.formURL,
+        });
       });
+    }
   },
 };
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -19,6 +19,24 @@ export default {
       formDocType: this.$route.query.formDocType,
       formURL: this.$route.query.formURL,
     });
+  },
+  mounted() {
+    woff
+      .init({
+        woffId: "6M4D9iS1o3oATtHNVJjS8w", // 발행된 WOFF ID
+      })
+      .then(() => {
+        if (!woff.isLoggedIn()) {
+          //로그인 유무 확인
+          window.alert("네이버웍스 로그인 후 사용 가능합니다.");
+          woff.closeWindow();
+          return;
+        }
+      })
+      .catch((err) => {
+        // 초기화 처리 중 오류가 발생한 경우
+        console.log(err.code, err.message);
+      });
 
     this.$router.push("/approval");
   },
