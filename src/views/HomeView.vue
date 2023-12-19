b35a028e7aa20c44b6ee5275055ea52dd10a3aa3
mououkouou <flag326@hufs.ac.kr>
2023-12-19 14:03:56 +0900

    test pc version

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,71 +10,36 @@ export default {
   computed: {
     ...mapState("approvalStore", ["docInfo"]),
   },
-  data: () => ({
-    service: "",
-    webUrl: "",
-  }),
   methods: {
     ...mapMutations("approvalStore", ["set_doc_info"]),
   },
+  created() {
+    this.set_doc_info({
+      docId: this.$route.query.docId,
+      formDocType: this.$route.query.formDocType,
+      formURL: this.$route.query.formURL,
+    });
+  },
   mounted() {
     /* eslint-disable */
-    this.service = this.$route.query.service;
-    console.log(this.$route.query);
-    switch (this.service) {
-      case "approval":
-        this.set_doc_info({
-          docId: this.$route.query.docId,
-          formDocType: this.$route.query.formDocType,
-          formURL: this.$route.query.formURL,
-        });
-        this.webUrl =
-          "https://gw.aekyung.kr/myoffice/ezApproval/formContainer/contDocView_Cross.aspx?DocID=" +
-          this.docInfo.docId +
-          "&DocHref=" +
-          this.docInfo.formURL +
-          "&formID=2021000191&orgDocid=&DocState=" +
-          this.docInfo.formDocType;
-        console.log("1");
-        break;
-
-      case "calendar":
-        this.webUrl = "https://gw.aekyung.kr?ownerId=";
-        break;
-    }
-
-    const connectOS = woff.getOS();
-    console.log(this.webUrl !== "");
-    console.log(this.webUrl);
-
-    if (connectOS === "web" && this.webUrl !== "") {
-      woff.openWindow({
-        url: this.webUrl,
+    woff
+      .init({
+        woffId: "6M4D9iS1o3oATtHNVJjS8w", // 발행된 WOFF ID
+      })
+      .then(() => {
+        if (!woff.isLoggedIn()) {
+          //로그인 유무 확인
+          window.alert("네이버웍스 로그인 후 사용 가능합니다.");
+          woff.closeWindow();
+          return;
+        }
+      })
+      .catch((err) => {
+        // 초기화 처리 중 오류가 발생한 경우
+        console.log(err.code, err.message);
       });
-      return;
-    } else if (connectOS === "ios" || connectOS === "android") {
-      woff
-        .init({
-          woffId: "6M4D9iS1o3oATtHNVJjS8w", // 발행된 WOFF ID
-        })
-        .then(() => {
-          if (!woff.isLoggedIn()) {
-            //로그인 유무 확인
-            window.alert("네이버웍스 로그인 후 사용 가능합니다.");
-            woff.closeWindow();
-            return;
-          } else if (this.service === undefined || this.service === "") {
-            window.alert("잘못된 주소 입니다.");
-            woff.closeWindow();
-          }
-        })
-        .catch((err) => {
-          // 초기화 처리 중 오류가 발생한 경우
-          console.log(err.code, err.message);
-        });
 
-      this.$router.push(this.service);
-    }
+    this.$router.push("/approval");
   },
 };
 </script>
