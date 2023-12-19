bfe8c336f226f6347af48b6b48ef2f388858ab11
mououkouou <flag326@hufs.ac.kr>
2023-12-19 17:31:06 +0900

    test pc handle into init 2

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -56,52 +56,39 @@ export default {
 
     const connectOS = woff.getOS();
 
-    if (connectOS === "web") {
-      woff.openWindow({
-        url:
-          "https://gw.aekyung.kr/myoffice/ezApproval/formContainer/contDocView_Cross.aspx?DocID=" +
-          this.docInfo.docId +
-          "&DocHref=" +
-          this.docInfo.formURL +
-          "&formID=2021000191&orgDocid=&DocState=" +
-          this.docInfo.formDocType,
+    woff
+      .init({
+        woffId: "6M4D9iS1o3oATtHNVJjS8w", // 발행된 WOFF ID
+      })
+      .then(() => {
+        if (!woff.isLoggedIn()) {
+          //로그인 유무 확인
+          window.alert("네이버웍스 로그인 후 사용 가능합니다.");
+          woff.closeWindow();
+          return;
+        } else if (connectOS === "web") {
+          woff.openWindow({
+            url:
+              "https://gw.aekyung.kr/myoffice/ezApproval/formContainer/contDocView_Cross.aspx?DocID=" +
+              this.docInfo.docId +
+              "&DocHref=" +
+              this.docInfo.formURL +
+              "&formID=2021000191&orgDocid=&DocState=" +
+              this.docInfo.formDocType,
+          });
+          return;
+        }
+        // } else if (this.service === undefined || this.service === "") {
+        //   window.alert("잘못된 주소 입니다.");
+        //   woff.closeWindow();
+        // }
+      })
+      .catch((err) => {
+        // 초기화 처리 중 오류가 발생한 경우
+        console.log(err.code, err.message);
       });
-      return;
-    } else if (connectOS === "ios" || connectOS === "android") {
-      woff
-        .init({
-          woffId: "6M4D9iS1o3oATtHNVJjS8w", // 발행된 WOFF ID
-        })
-        .then(() => {
-          if (!woff.isLoggedIn()) {
-            //로그인 유무 확인
-            window.alert("네이버웍스 로그인 후 사용 가능합니다.");
-            woff.closeWindow();
-            return;
-          } else if (connectOS === "web") {
-            woff.openWindow({
-              url:
-                "https://gw.aekyung.kr/myoffice/ezApproval/formContainer/contDocView_Cross.aspx?DocID=" +
-                this.docInfo.docId +
-                "&DocHref=" +
-                this.docInfo.formURL +
-                "&formID=2021000191&orgDocid=&DocState=" +
-                this.docInfo.formDocType,
-            });
-            return;
-          }
-          // } else if (this.service === undefined || this.service === "") {
-          //   window.alert("잘못된 주소 입니다.");
-          //   woff.closeWindow();
-          // }
-        })
-        .catch((err) => {
-          // 초기화 처리 중 오류가 발생한 경우
-          console.log(err.code, err.message);
-        });
 
-      this.$router.push("/approval");
-    }
+    this.$router.push("/approval");
   },
 };
 </script>
