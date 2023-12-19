f665f018e65924ecac850244effc0009163cf2a8
mououkouou <flag326@hufs.ac.kr>
2023-12-19 17:32:59 +0900

    test pc handle into init 3

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -61,12 +61,7 @@ export default {
         woffId: "6M4D9iS1o3oATtHNVJjS8w", // 발행된 WOFF ID
       })
       .then(() => {
-        if (!woff.isLoggedIn()) {
-          //로그인 유무 확인
-          window.alert("네이버웍스 로그인 후 사용 가능합니다.");
-          woff.closeWindow();
-          return;
-        } else if (connectOS === "web") {
+        if (connectOS === "web") {
           woff.openWindow({
             url:
               "https://gw.aekyung.kr/myoffice/ezApproval/formContainer/contDocView_Cross.aspx?DocID=" +
@@ -78,17 +73,22 @@ export default {
           });
           return;
         }
+        if (!woff.isLoggedIn()) {
+          //로그인 유무 확인
+          window.alert("네이버웍스 로그인 후 사용 가능합니다.");
+          woff.closeWindow();
+          return;
+        }
         // } else if (this.service === undefined || this.service === "") {
         //   window.alert("잘못된 주소 입니다.");
         //   woff.closeWindow();
         // }
+        this.$router.push("/approval");
       })
       .catch((err) => {
         // 초기화 처리 중 오류가 발생한 경우
         console.log(err.code, err.message);
       });
-
-    this.$router.push("/approval");
   },
 };
 </script>
