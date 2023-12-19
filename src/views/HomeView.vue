381760a4ac214a0fdba987aa0503e562b85c0ebe
mououkouou <flag326@hufs.ac.kr>
2023-12-19 09:17:46 +0900

    add branch OS

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -43,24 +43,32 @@ export default {
   },
   mounted() {
     /* eslint-disable */
-    woff
-      .init({
-        woffId: "6M4D9iS1o3oATtHNVJjS8w", // 발행된 WOFF ID
-      })
-      .then(() => {
-        if (!woff.isLoggedIn()) {
-          //로그인 유무 확인
-          window.alert("네이버웍스 로그인 후 사용 가능합니다.");
-          woff.closeWindow();
-          return;
-        }
-      })
-      .catch((err) => {
-        // 초기화 처리 중 오류가 발생한 경우
-        console.log(err.code, err.message);
+    const connectOS = woff.getOS();
+    if (connectOS === "web" && this.webUrl !== "") {
+      woff.openWindow({
+        url: this.webUrl,
       });
+      return;
+    } else if (connectOS === "ios" || connectOS === "android") {
+      woff
+        .init({
+          woffId: "6M4D9iS1o3oATtHNVJjS8w", // 발행된 WOFF ID
+        })
+        .then(() => {
+          if (!woff.isLoggedIn()) {
+            //로그인 유무 확인
+            window.alert("네이버웍스 로그인 후 사용 가능합니다.");
+            woff.closeWindow();
+            return;
+          }
+        })
+        .catch((err) => {
+          // 초기화 처리 중 오류가 발생한 경우
+          console.log(err.code, err.message);
+        });
 
-    this.$router.push(this.service);
+      this.$router.push(this.service);
+    }
   },
 };
 </script>
