d334f51e0b7b931678c8bbdad34507bd946a5e44
mououkouou <flag326@hufs.ac.kr>
2023-12-19 09:07:03 +0900

    test change path

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -17,37 +17,38 @@ export default {
   methods: {
     ...mapMutations("approvalStore", ["set_doc_info"]),
   },
-  mounted() {
-    /* eslint-disable */
-    this.service = this.$route.query.service;
-    switch (this.service) {
-      case "approval":
-        this.set_doc_info({
-          docId: this.$route.query.docId,
-          formDocType: this.$route.query.formDocType,
-          formURL: this.$route.query.formURL,
-        });
-        this.webUrl =
-          "https://gw.aekyung.kr/myoffice/ezApproval/formContainer/contDocView_Cross.aspx?DocID=" +
-          this.docInfo.docId +
-          "&DocHref=" +
-          this.docInfo.formURL +
-          "&formID=2021000191&orgDocid=&DocState=" +
-          this.docInfo.formDocType;
-        break;
-
-      case "calendar":
-        this.webUrl = "https://gw.aekyung.kr?ownerId=";
-        break;
-
-      default:
-        window.alert("잘못된 url 형식입니다.");
-        woff.closeWindow();
-    }
-
-    this.$router.push(this.service);
-  },
   created() {
+    /* eslint-disable */
+    window.alert(this.$route.query.woff.state);
+    if (this.$route.query.woff.state === undefined) {
+      this.service = this.$route.query.service;
+      switch (this.service) {
+        case "approval":
+          this.set_doc_info({
+            docId: this.$route.query.docId,
+            formDocType: this.$route.query.formDocType,
+            formURL: this.$route.query.formURL,
+          });
+          this.webUrl =
+            "https://gw.aekyung.kr/myoffice/ezApproval/formContainer/contDocView_Cross.aspx?DocID=" +
+            this.docInfo.docId +
+            "&DocHref=" +
+            this.docInfo.formURL +
+            "&formID=2021000191&orgDocid=&DocState=" +
+            this.docInfo.formDocType;
+          break;
+
+        case "calendar":
+          this.webUrl = "https://gw.aekyung.kr?ownerId=";
+          break;
+
+        default:
+          window.alert("잘못된 url 형식입니다.");
+          woff.closeWindow();
+      }
+    }
+  },
+  mounted() {
     /* eslint-disable */
     woff
       .init({
@@ -65,6 +66,8 @@ export default {
         // 초기화 처리 중 오류가 발생한 경우
         console.log(err.code, err.message);
       });
+
+    this.$router.push(this.service);
   },
 };
 </script>
